3b0d0139e3faab0f4cb3996c1d8e1e335a71fbb8
LOUKHNATI Mohamed Khalil <medkha.bt@gmail.com>
2024-05-08 21:07:52 +0200

    clean up before installation and fix some problems

--- install.sh
+++ install.sh
@@ -1,17 +1,27 @@
 #! /bin/bash
 
-# Maybe the installer in the release 
-## Don't forgot the version.
+
+clean(){
+    rm -rf $HOME/Library/Job-apply
+    cp $HOME/.bashrc $HOME/.back_up_bashrc
+    grep -v '\#job-apply-snippet' $HOME/.bashrc > $HOME/.tmp_bashrc
+    mv $HOME/.tmp_bashrc $HOME/.bashrc
+    rm /usr/local/bin/job-apply*
+}
+
+
+echo "cleaning.."
+clean
+
+echo "end of cleaning"
 
 mkdir ~/Library/Job-apply
 
-version='1.0'
-## Set the job_apply path ( static first than maybe also make it dynamic)
-project_path="$HOME/Library/Job-apply/$version"
+version='alpha-0.2.0'
 
+project_path="$HOME/Library/Job-apply/$version"
 mkdir $project_path
 
-## put the path in library ? 
 cd $project_path;
 git clone https://github.com/medkhabt/coverletters.git . 
 
@@ -19,7 +29,6 @@ rm TODO.md 2> /dev/null
 rm README.md 2> /dev/null
 rm install.sh 2> /dev/null
 
-## mv command to the /bin
 mv job-apply-core /usr/local/bin/job-apply-core-$version
 mv job-apply /usr/local/etc/bash_completion.d/
 
@@ -28,8 +37,11 @@ mkdir applications
 
 ## Check first if they don't exist in the bashrc file.
 
-echo "export job_apply_path='$project_path'" >> ~/.bashrc
-echo "alias job-apply='. job-apply-core-$version'" >> ~/.bashrc
-
+echo "# Don't remove the the comments #job-apply-snippet , important for the cleaning/check existance of the .bashrc file of job-apply-snippets" >> $HOME/.bashrc
+echo ". /usr/local/etc/bash_completion.d/job-apply #job-apply-snippet" >> $HOME/.bashrc 
+echo "export job_apply_path='$project_path' #job-apply-snippet" >> $HOME/.bashrc
+echo "alias job-apply='. job-apply-core-$version' #job-apply-snippet" >> $HOME/.bashrc
 
 ## mv the completion command 
+
+
