be4728f111de357293e4fe0c9601a3679cdde8b6
LOUKHNATI Mohamed Khalil <medkha.bt@gmail.com>
2024-05-08 18:55:06 +0200

    Fixing env variable, and adding some changes to the installation script

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 # Maybe the installer in the release 
 ## Don't forgot the version.
 
-mkdir ~/Library/job-apply
+mkdir ~/Library/Job-apply
 
-version=1.0
+version='1.0'
 ## Set the job_apply path ( static first than maybe also make it dynamic)
-project_path="~/Library/job-apply/$version"
+project_path="~/Library/Job-apply/$version"
 
-mkdir $project_path
+#mkdir $project_path
 
 ## put the path in library ? 
 
@@ -20,12 +20,12 @@ rm $project_path/README.md 2> /dev/null
 rm $project_path/install.sh 2> /dev/null
 
 ## mv command to the /bin
-mv $project_path/job-apply-core /usr/bin 
+mv $project_path/job-apply-core /usr/local/bin/job-apply-core-$version
 mv $project_path/job-apply /usr/local/etc/bash_completion.d/
 
 
-echo "export job_apply_path=$project_path" >> ~/.bashrc
-echo "alias job-apply='. job-apply-core'" >> ~/.bashrc
+echo "export job_apply_path='$project_path'" >> ~/.bashrc
+echo "alias job-apply='. job-apply-core-$version'" >> ~/.bashrc
 
 
 ## mv the completion command 
